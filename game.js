1552ffd00bd203b731488e1d4fd220d4181964c8
Mackenzie <ma.luzarow@gmail.com>
2018-01-10 21:05:59 -0600

    Corrected init code.

--- game.js
+++ game.js
@@ -1,40 +1,41 @@
 class TictactoeController {
-    static initialize () {
-        // Gather all cells into matrix
-        this.cells = [];
-        var rows = document.getElementsByClassName ("row");
-        
-        for (var i = 0; i < rows.length; i++) {
-            for (var k = 0; k < 3; k++) {
-                this.cells[i][k] = new TictactoeCell (rows[i][k]);
-                )
-            }
-        }
-    }
+	static initialize () {
+		// Gather all cells into matrix
+		this.cells = [[], [], []];
+		var rows = document.getElementsByClassName ("row");
+		
+		for (var i = 0; i < 3; i++) {
+			var row = rows[i].getElementsByClassName ("cell");
+			
+			for (var k = 0; k < 3; k++) {
+				this.cells[i][k] = new TictactoeCell (row[k]);
+			}
+		}
+	}
 }
 
 class TictactoeCell {
-    constructor (element) {
-        this.DOMelement = element;
-        this.DOMelement.addEventListener ("click", this.onClick.bind (this));
-        this.flagTaken = false;
-        this.ownedBy = null;
-    }
-    
-    setOwned (side) {
-        this.flagTaken = true;
-        this.ownedBy = side;
-    }
-    
-    isTaken () {
-        return (this.flagTaken);
-    }
-    
-    owner () {
-        return (this.ownedBy);
-    }
-    
-    onClick () {
-        alert ("Yay");
-    }
+	constructor (element) {
+		this.DOMelement = element;
+		this.DOMelement.addEventListener ("click", this.onClick.bind (this));
+		this.flagTaken = false;
+		this.ownedBy = null;
+	}
+	
+	setOwned (side) {
+		this.flagTaken = true;
+		this.ownedBy = side;
+	}
+	
+	isTaken () {
+		return (this.flagTaken);
+	}
+	
+	owner () {
+		return (this.ownedBy);
+	}
+	
+	onClick () {
+		alert ("Yay");
+	}
 }
